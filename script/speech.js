d1f7ac5378ab55b1231b9f6bb26b26d7cbcc7c84
Andy Giannini <andy@onedesigncompany.com>
2016-02-26 15:37:07 -0600

    Make speech recognition run continuously

--- script/speech.js
+++ script/speech.js
@@ -1,1 +1,24 @@
-// speech capture events go here
+//(function() {
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = false;
+    recognition.interimResults = false;
+
+    recognition.onerror = function(event) {
+        console.log(event);
+    }
+
+    recognition.onend = function(e) {
+        recognition.start();
+    }
+
+    recognition.onresult = function(event) {
+        var string = '';
+        for (var i = event.resultIndex; i < event.results.length; ++i) {
+            string += event.results[i][0].transcript;
+        }
+        speechText.set(string);
+        console.log(string);
+    }
+
+    recognition.start();
+//})();
